5714799ff7ee27977ae5dd9a7260542a2c714035
_arka_ <suryasantosh14523@gmail.com>
2019-07-05 23:11:09 +0530

    Front End Completed

---
+++ index.php
@@ -0,0 +1,238 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <!-- FontAwesome Script -->
+    <script src="https://kit.fontawesome.com/7b3e632a6c.js"></script>
+    <!-- Optional CSS -->
+    <link rel="stylesheet" type="text/css" href="css/index.css">
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+    <title>MySite - Untitled</title>
+  </head>
+  <body>
+    
+    <div class="main-container">
+
+        <header>
+
+            <!-- Navbar -->
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                <a class="navbar-brand ml-2" href="#"><i class="fas fa-meteor mr-2"></i>MySite</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ml-auto mr-3">
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <i class="fas fa-user-circle mr-2"></i>UserName
+                            </a>
+                            <div class="dropdown-menu ml-n3" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="#"><i class="fas fa-cog mr-2"></i>Settings</a>
+                                <a class="dropdown-item" href="#"><i class="fas fa-bell mr-2"></i>Notifications</a>
+                            <div class="dropdown-divider"></div>
+                                <a class="dropdown-item" href="#"><i class="fas fa-sign-out-alt mr-2"></i>Log Out</a>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+
+        </header>
+
+        <section>
+            
+            <div style="width:150px"></div>
+            
+
+            <!-- Quick Links Section -->
+
+            <div class="filters-section">
+                <h4 class="mr-auto">Quick Filters</h4>
+                <hr>
+                <ul class="list-group mb-3">
+                    <li class="list-group-item list-group-item-success">Filters</li>
+                    <li class="list-group-item list-group-item-filter">Department</li>
+                    <li class="list-group-item list-group-item-filter">Socities</li>
+                    <li class="list-group-item list-group-item-filter">Events</li>
+                </ul>
+                <ul class="list-group">
+                    <li class="list-group-item list-group-item-warning">Upload Time</li>
+                    <li class="list-group-item list-group-item-filter">Last Hour</li>
+                    <li class="list-group-item list-group-item-filter">This Day</li>
+                    <li class="list-group-item list-group-item-filter">This Week</li>
+                    <li class="list-group-item list-group-item-filter">This Month</li>
+                </ul>
+                <div style="height:20px"></div>
+                <ul class="list-group">
+                  <li class="list-group-item list-group-item-filter">Total Posts posted today<span class="badge badge-pill badge-secondary ml-2">25</span></li>
+                </ul>
+                <div style="height: 50px"></div>
+            </div> 
+
+            <div style="width:50px"></div>
+
+            <!-- Posts Section -->
+            
+            <div class="posts-section">
+                <div class="card" style="width: 35rem;">
+                    <div class="card-header">Pikachu</div>
+                    <img src="assets/images/pikachu.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="card-link">Card link</a>
+                        <a href="#" class="card-link">Another link</a>
+                    </div>
+                    <div class="card-footer text-muted">
+                        2 days ago
+                    </div>
+                </div>
+                <div class="card" style="width: 35rem;">
+                    <div class="card-header">Minion</div>
+                    <img src="assets/images/minion.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="card-link">Card link</a>
+                        <a href="#" class="card-link">Another link</a>
+                    </div>
+                    <div class="card-footer text-muted">
+                        2 days ago
+                    </div>
+                </div> 
+                <div class="card" style="width: 35rem;">
+                    <div class="card-header">Kuroko</div>
+                    <img src="assets/images/kuroko.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="card-link">Card link</a>
+                        <a href="#" class="card-link">Another link</a>
+                    </div>
+                    <div class="card-footer text-muted">
+                        2 days ago
+                    </div>
+                </div> 
+                <div class="card" style="width: 35rem;">
+                    <div class="card-header">John Wick</div>
+                    <img src="assets/images/john-wick.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="card-link">Card link</a>
+                        <a href="#" class="card-link">Another link</a>
+                    </div>
+                    <div class="card-footer text-muted">
+                        2 days ago
+                    </div>
+                </div> 
+                <div class="card" style="width: 35rem;">
+                    <div class="card-header">Uncharted 4</div>
+                    <img src="assets/images/uncharted-4.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="card-link">Card link</a>
+                        <a href="#" class="card-link">Another link</a>
+                    </div>
+                    <div class="card-footer text-muted">
+                        2 days ago
+                    </div>
+                </div> 
+                <div class="card" style="width: 35rem;">
+                    <div class="card-header">Need for Speed</div>
+                    <img src="assets/images/nfs.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="card-link">Card link</a>
+                        <a href="#" class="card-link">Another link</a>
+                    </div>
+                    <div class="card-footer text-muted">
+                        2 days ago
+                    </div>
+                </div> 
+            </div>
+
+            <div style="width: 50px"></div>
+
+            <!-- profile section -->
+
+            <div class="profile-section">
+                <li class="list-group-item mb-3" id="upload-post" style="width: 300px">
+                    <i class="fas fa-plus mr-3"></i>Upload Post
+                </li>
+                <div class="card" style="width: 300px;">
+                  <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  </div>
+                </div>
+                <hr style="width: 300px; margin-left: 0">
+                <ul class="list-group" style="width: 300px">
+                  <li class="list-group-item"><i class="fas fa-upload mr-2"></i>Posts Uploaded<span class="badge badge-pill badge-secondary ml-3">200</span></li>
+                  <li class="list-group-item"><i class="far fa-calendar-check mr-2"></i>UpComing Events</li>
+                  <li class="list-group-item"><i class="fas fa-link mr-2"></i>Website Link</li>
+                </ul>
+                <li class="list-group-item mt-3" style="width: 300px">Posts Uploaded Today<span class="badge badge-pill badge-secondary ml-3">2</span></li>
+            </div>
+
+            <div style="width:50px"></div>
+
+        </section>
+        
+    </div>
+
+    <div class="container new-post-container">
+        <div class=" card mx-auto my-5">
+            <div class="ml-auto mr-2 mt-2" id="discard-post"><i class="fas fa-times"></i></div>
+          <div class="card-body">
+            <form>
+              <div class="form-group">
+                <label for="title">Title for the Post</label>
+                <input type="text" class="form-control" id="title" aria-describedby="emailHelp" placeholder="Enter Title">
+                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+              </div>
+              <div class="form-group">
+                <label for="file">Upload Image</label>
+                <input type="file" class="form-control-file" id="file">
+              </div>
+              <p>Add Tags</p>
+              <div class="form-group form-check">
+                <input type="checkbox" class="form-check-input" id="tag1">
+                <label class="form-check-label" for="tag1">Department</label>
+              </div>
+              <div class="form-group form-check">
+                <input type="checkbox" class="form-check-input" id="tag2">
+                <label class="form-check-label" for="tag2">Society</label>
+              </div>
+              <div class="form-group form-check">
+                <input type="checkbox" class="form-check-input" id="tag3">
+                <label class="form-check-label" for="tag3">Events</label>
+              </div>
+              <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+          </div>
+        </div>
+    </div>
+
+    <!-- Optional JavaScript -->
+
+    <script type="text/javascript">
+        $('#upload-post').on('click', function(){
+            $('.main-container').css('opacity' , 0.05);
+            $('.new-post-container').show();
+        })
+        $('#discard-post').on('click', function(){
+            $('.new-post-container').hide();
+            $('.main-container').css('opacity', 1);
+        })
+    </script>
+
+  </body>
+</html>
