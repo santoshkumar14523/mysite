b9ecdd148ddce8206e14e5abfe05bddbe06e3fa2
_arka_ <suryasantosh14523@gmail.com>
2019-07-07 14:15:37 +0530

    full ui completed but not to full extent

--- index.php
+++ index.php
@@ -59,23 +59,123 @@
             <div class="filters-section">
                 <h4 class="mr-auto">Quick Filters</h4>
                 <hr>
-                <ul class="list-group mb-3">
+                <!-- <ul class="list-group my-3">
                     <li class="list-group-item list-group-item-success list-group-item-filter-title">Filters</li>
                     <li class="list-group-item list-group-item-filter" value="dep">Department</li>
                     <li class="list-group-item list-group-item-filter" value="soc">Socities</li>
                     <li class="list-group-item list-group-item-filter" value="evnt">Events</li>
+                </ul> -->
+                    <!-- <ul class="list-group">
+                        <li class="list-group-item list-group-item-warning">Upload Time</li>
+                        <li class="list-group-item list-group-item-filter">Last Hour</li>
+                        <li class="list-group-item list-group-item-filter">This Day</li>
+                        <li class="list-group-item list-group-item-filter">This Week</li>
+                        <li class="list-group-item list-group-item-filter">This Month</li>
+                    </ul> -->
+                <ul class="list-group mb-3">
+                    <li class="list-group-item list-group-item-success">Filters</li>
+                    <li class="list-group-item dropdown-toggle" href="#depfilters" data-toggle="collapse">Department
+                    </li>
+                    <ul class="collapse list-unstyled list-group" id="depfilters">
+                        <li class="list-group-item">
+                            <div class="form-check">
+                              <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+                              <label class="form-check-label" for="defaultCheck1">
+                                ECE
+                              </label>
+                            </div>
+                            <div class="form-check">
+                              <input class="form-check-input" type="checkbox" value="" id="defaultCheck2">
+                              <label class="form-check-label" for="defaultCheck2">
+                                CSE
+                              </label>
+                            </div>
+                            <div class="form-check">
+                              <input class="form-check-input" type="checkbox" value="" id="defaultCheck3">
+                              <label class="form-check-label" for="defaultCheck3">
+                                Arch.
+                              </label>
+                            </div>
+                        </li>
+                    </ul>
+                    <li class="list-group-item dropdown-toggle" href="#socfilters" data-toggle="collapse">Societies
+                    </li>
+                    <ul class="collapse list-unstyled list-group" id="socfilters">
+                        <li class="list-group-item">
+                            <div class="form-check">
+                              <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+                              <label class="form-check-label" for="defaultCheck1">
+                                TDS
+                              </label>
+                            </div>
+                            <div class="form-check">
+                              <input class="form-check-input" type="checkbox" value="" id="defaultCheck2">
+                              <label class="form-check-label" for="defaultCheck2">
+                                TCA
+                              </label>
+                            </div>
+                            <div class="form-check">
+                              <input class="form-check-input" type="checkbox" value="" id="defaultCheck3">
+                              <label class="form-check-label" for="defaultCheck3">
+                                KOSS
+                              </label>
+                            </div>
+                        </li>
+                    </ul>
+                    <li class="list-group-item dropdown-toggle" href="#evntfilters" data-toggle="collapse">Events
+                    </li>
+                    <ul class="collapse list-unstyled list-group" id="evntfilters">
+                        <li class="list-group-item">
+                            <div class="form-check">
+                              <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+                              <label class="form-check-label" for="defaultCheck1">
+                                Fests
+                              </label>
+                            </div>
+                            <div class="form-check">
+                              <input class="form-check-input" type="checkbox" value="" id="defaultCheck2">
+                              <label class="form-check-label" for="defaultCheck2">
+                                Guest Lectures
+                              </label>
+                            </div>
+                            <div class="form-check">
+                              <input class="form-check-input" type="checkbox" value="" id="defaultCheck3">
+                              <label class="form-check-label" for="defaultCheck3">
+                                Tournaments
+                              </label>
+                            </div>
+                        </li>
+                    </ul>
+                    <li class="list-group-item dropdown-toggle" href="#uploadTimeFilters" data-toggle="collapse">Upload Time
+                    </li>
+                    <ul class="collapse list-unstyled list-group" id="uploadTimeFilters">
+                        <li class="list-group-item">
+                            <div class="form-check">
+                              <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+                              <label class="form-check-label" for="defaultCheck1">
+                                Last Hour
+                              </label>
+                            </div>
+                            <div class="form-check">
+                              <input class="form-check-input" type="checkbox" value="" id="defaultCheck2">
+                              <label class="form-check-label" for="defaultCheck2">
+                                This Day
+                              </label>
+                            </div>
+                            <div class="form-check">
+                              <input class="form-check-input" type="checkbox" value="" id="defaultCheck3">
+                              <label class="form-check-label" for="defaultCheck3">
+                                This Week
+                              </label>
+                            </div>
+                        </li>
+                    </ul>
                 </ul>
-                <ul class="list-group">
-                    <li class="list-group-item list-group-item-warning">Upload Time</li>
-                    <li class="list-group-item list-group-item-filter">Last Hour</li>
-                    <li class="list-group-item list-group-item-filter">This Day</li>
-                    <li class="list-group-item list-group-item-filter">This Week</li>
-                    <li class="list-group-item list-group-item-filter">This Month</li>
-                </ul>
-                <div style="height:20px"></div>
                 <ul class="list-group">
                   <li class="list-group-item list-group-item-filter">Total Posts posted today<span class="badge badge-pill badge-secondary ml-2">25</span></li>
                 </ul>
+                <div style="height:20px"></div>
+                
                 <div style="height: 50px"></div>
             </div> 
 
@@ -119,13 +219,12 @@
 
     <div class="container new-post-container">
         <div class=" card mx-auto my-5">
-            <div class="ml-auto mr-2 mt-2" id="discard-post"><i class="fas fa-times"></i></div>
-          <div class="card-body">
+            <div class="ml-auto mr-2 mb-2" id="discard-post"><i class="fas fa-times"></i></div>
+          <div class="card-body mt-2">
             <form method="POST" action="config/upload.php" enctype="multipart/form-data">
               <div class="form-group">
                 <label for="title">Title for the Post</label>
                 <input type="text" class="form-control" name="title" id="title" aria-describedby="emailHelp" placeholder="Enter Title" required>
-                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
               </div>
               <div class="form-group">
                 <label for="descrptn">Description for the Post</label>
@@ -140,13 +239,28 @@
                 <input type="file" name="file" class="form-control-file" id="file">
               </div>
               <p>Add Tags</p>
-              <!-- <select name="depfilters" class="custom-select">
-                <option selected>Open this select menu</option>
-                <option value="dep1">One</option>
-                <option value="dep2">Two</option>
-                <option value="dep3">Three</option>
-              </select> -->
-              <div class="form-group form-check">
+              <div class="select-filters form-group">
+                <select name="depfilters" class="custom-select">
+                    <option selected>Department</option>
+                    <option value="dep1">One</option>
+                    <option value="dep2">Two</option>
+                    <option value="dep3">Three</option>
+                </select>
+                <select name="depfilters" class="custom-select">
+                    <option selected>Society</option>
+                    <option value="dep1">One</option>
+                    <option value="dep2">Two</option>
+                    <option value="dep3">Three</option>
+                </select>
+                <select name="depfilters" class="custom-select">
+                    <option selected>Events</option>
+                    <option value="dep1">One</option>
+                    <option value="dep2">Two</option>
+                    <option value="dep3">Three</option>
+                </select>
+              </div>
+
+              <!-- div class="form-group form-check">
                 <input type="checkbox" class="form-check-input" name='filters' value='dep' id="tag1">
                 <label class="form-check-label" for="tag1">Department</label>
               </div>
@@ -157,8 +271,9 @@
               <div class="form-group form-check">
                 <input type="checkbox" class="form-check-input" name='filters' value='evnt' id="tag3">
                 <label class="form-check-label" for="tag3">Events</label>
-              </div>
-              <button type="submit" name="upload" class="btn btn-primary">Submit</button>
+              </div> -->
+              <button type="submit" name="upload" class="btn btn-primary">Upload Post</button>
+              <button class="btn btn-light">Discard Post</button>
             </form>
           </div>
         </div>
@@ -178,6 +293,9 @@
         })
         $('.list-group-item-filter').on('click', function(){
             $('.list-group-item-filter-title').html('Clear Filters');
+            $(".list-group-item-filter-title").removeClass('list-group-item-success');
+            $(".list-group-item-filter-title").addClass('list-group-item-warning');
+            $(".list-group-item-filter-title").addClass("clear-filters");
             var value = $(this).attr('value');
             $.ajax({
                 type: "POST",
@@ -193,6 +311,10 @@
         $('.list-group-item-filter-title').on('click', function(){
             $('.posts-section').load("config/allposts.php");
             $(this).html('Filters');
+            $(".list-group-item-filter-title").removeClass('list-group-item-warning');
+            $(".list-group-item-filter-title").removeClass('clear-filters');
+            $(".list-group-item-filter-title").addClass('list-group-item-success');
+
         })
     </script>
 
